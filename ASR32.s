b485c069c6f970dbdefa5a1959c2b8b3c41c0aee
Benjamin Niccum <113940036+Benjination@users.noreply.github.com>
2023-09-08 15:57:28 -0500

    Initial Commit

---
+++ ASR32.s
@@ -0,0 +1,11 @@
+.global ASR32
+.text
+
+// int add32(int x, int y)
+// Input: x in R0, y in R1
+// Output: Result in R0
+
+ASR32:
+    ASR R0, R1  // Right shift R0 by the number of bits specified in R1
+    BX LR      // Return with the result in R0
+    
