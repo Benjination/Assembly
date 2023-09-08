05534018fc967d6f417cc0d5e2a04f750fe97e03
Benjamin Niccum <113940036+Benjination@users.noreply.github.com>
2023-09-08 15:03:05 -0500

    Updated

--- LSR32.s
+++ LSR32.s
@@ -1,11 +1,11 @@
-.global LSR32
+ .global LSR32
 .text
 
 // int LSL32(int x, int y)
 // Input: x in R0, y in R1
 // Output: Result in R0
 
-add32:
+LSR32:
     LSR R0, R1  // Left shift R0 by the number of bits specified in R1
     BX LR      // Return with the result in R0
     
