ca7b5de08209b613c1df3e0a3cd3a4856cd6f0f5
Benjamin Niccum <113940036+Benjination@users.noreply.github.com>
2023-09-08 15:02:12 -0500

    Works with LSL and LSR

--- ARM32.c
+++ ARM32.c
@@ -1,5 +1,8 @@
+//Benjamin Niccum btn1609@mavs.uta.edu
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 // Declare the external assembly function
 extern u_int32_t LSL32(u_int32_t x, u_int32_t y);
@@ -7,31 +10,36 @@ extern u_int32_t LSR32(u_int32_t x, u_int32_t y);
 
 int main()
 {
+    
+
     int select;
     printf("Select an option\n");
     printf("1. LSL (Right BitShift)\n2. LSR (Left Bitshift)\n");
-    scanf("%d", select);
+    scanf("%d", &select);
     
     u_int32_t x, y, result;
     
     switch(select)
     {
         case 1:
-            printf("Enter decimal number to be shifted");
-            scanf("%d", x);
-            printf("Enter number of bits to shift");
-            scanf("%d", y);
+            printf("Enter decimal number to be shifted\n");
+            scanf("%u", &x);
+            printf("Enter number of bits to shift\n");
+            scanf("%u", &y);
             result = LSL32(x, y);
+            break;
         case 2:
-            printf("Enter decimal number to be shifted");
-            scanf("%d", x);
-            printf("Enter number of bits to shift");
-            scanf("%d", y);
+            printf("Enter decimal number to be shifted\n");
+            scanf("%u", &x);
+            printf("Enter number of bits to shift\n");
+            scanf("%u", &y);
             result = LSR32(x, y);
+            break;
              
     }
-    
-    printf("%d\n", result); 
-
+        
+            printf("The result is: %u\n", result); 
+        
+       
     return 0;
 }
