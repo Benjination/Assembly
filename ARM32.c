efa0e94b8c05e9c0d6ccf7a25fd16e3fbbab46bc
Benjamin Niccum <113940036+Benjination@users.noreply.github.com>
2023-09-08 15:56:59 -0500

    Supports ASR and ASL

--- ARM32.c
+++ ARM32.c
@@ -7,6 +7,8 @@
 // Declare the external assembly function
 extern u_int32_t LSL32(u_int32_t x, u_int32_t y);
 extern u_int32_t LSR32(u_int32_t x, u_int32_t y);
+extern u_int32_t ASR32(u_int32_t x, u_int32_t y);
+extern u_int32_t ASL32(u_int32_t x, u_int32_t y);
 
 int main()
 {
@@ -14,7 +16,7 @@ int main()
 
     int select;
     printf("Select an option\n");
-    printf("1. LSL (Right BitShift)\n2. LSR (Left Bitshift)\n");
+    printf("1. LSL (Right BitShift)\n2. LSR (Left Bitshift)\n3. ASR (Arithmetic Shift Right)\n4. ASL (Just Use LSL)");
     scanf("%d", &select);
     
     u_int32_t x, y, result;
@@ -35,10 +37,27 @@ int main()
             scanf("%u", &y);
             result = LSR32(x, y);
             break;
-             
-    }
+        case 3:
+            printf("Enter decimal number to be shifted\n");
+            scanf("%u", &x);
+            printf("Enter number of bits to shift\n");
+            scanf("%u", &y);
+            result = ASR32(x, y);
+            break;
+         case 4:
+            printf("The result will be the same as LSL.\n");
+            printf("Enter decimal number to be shifted\n");
+            scanf("%u", &x);
+            printf("Enter number of bits to shift\n");
+            scanf("%u", &y);
+            result = LSL32(x, y);
+            break;
+            }
         
             printf("The result is: %u\n", result); 
+            char dump;
+            printf("Press Enter to end program");
+            scanf("%s", dump);
         
        
     return 0;
