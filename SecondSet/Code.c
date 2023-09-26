56d1cafe6574fe0ef4de6e2ffe8cbeb52226f94b
Benjamin Niccum <btn1609@mavs.uta.edu>
2023-09-26 13:50:32 -0500

    End of Class

--- SecondSet/Code.c
+++ SecondSet/Code.c
@@ -6,11 +6,13 @@
 extern int maxUint8(int x, int y);
 extern uint32_t StrLen(char s[20]);
 extern uint32_t StrLenSpec(char s[20], char n);
+extern uint32_t StrLenSpecInt(char s[20], char n, int m);
 
 int main()
 {
     char s[20] = "Hello World";
     //s = "Hello World";
+    int m = 2; //nth number of letters in string
     int x = 8;
     int y = 16;
 
--- SecondSet/Code.s
+++ SecondSet/Code.s
@@ -32,3 +32,23 @@ Loop:
     BNE Loop     //If the branch is not zero, repeat. When string runs out of letters, Branch is equal to 0. No loop
     SUB R0, R3, #1 //Moves R1 minus 2 into R0
     BX LR          //Returns R0
+
+StrLenSpecInt: //Finds the length of part of a string to a specific letter
+//R0 Address in memory
+//R1 char n
+//R2 int x
+//R3 count
+//R4 iteration
+PUSH {R4} //Pushes R4 onto the stack LIFO
+MOV R3, #0 //Moves Zero into R2
+Loop:           //Exit conditions "Finds nth number of char in string", "Find End of String"
+    LDRSB R4, [R0, R3] //Load a character, Adds R1 to R0 (Moves bit right by the "Offset" in R1) Puts in R4
+    CMP R4, R1     //Check to see if it's zero
+    SUBEQ R2, R2, #1 //If the two symbols are equal, reduce the count m by 1.
+    ADD R3, R3, #1 //Adds one byte to R1 to move the address by one byte. 
+    CMP R2, #0     //Check if m has reduced to 0
+    BNE Loop     //If the branch is not zero, repeat. When string runs out of letters, Branch is equal to 0. No loop
+    CMP R4, #0
+    SUB R0, R3, #1 //Moves R1 minus 2 into R0
+    POP {R4}       //Removes access to register R4
+    BX LR          //Returns R0
