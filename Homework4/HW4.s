dcf25b9308db74ec1ef5979caaf4150dc828f556
Benjamin Niccum <btn1609@mavs.uta.edu>
2023-09-28 11:54:27 -0500

    isStrEqual ready for testing

--- Homework4/HW4.s
+++ Homework4/HW4.s
@@ -9,10 +9,25 @@
 .text
 
 isStrEqual:   //Determines if two strings are the same
-//R0 = Address of Str1, R1 = Address of Str2, R2 = Character to Test, R3 = Counter
-    MOV R3, #0
+//R0 = Address of Str1, R1 = Address of Str2, R2 = 1st Char to Test, R3 = 2nd char to test, PUSH R4 = Counter
+    PUSH {R4}            //Gives access to R4
+    MOV R4, #0           //R4 will be iterator, and is set to zero 
 LOOP:
-    LDRSB R1, [R2, R3]
+    LDRSB R2, [R0, R3]   //Loads first symbol of Str1 into R2, displaced by R4
+    LDRSB R3, [R1, R3]   //Loads first symbol of Str2 into R3, displaced by R4
+    CMP R2, R3           //Compares first two symbols
+    BNE FAIL             //Strings are not equal, jump to Fail label
+    CMP R3, #0           //Tests if symbol in R3 is NULL terminator indicating end of string
+    ADD R4, #1           //Moves counter to the next symbol in the string
+    BNE LOOP             //If R3 is not zero, program loops
+    MOV R0 #1            //Strings are equal
+    POP {R4}             //Removes access to R4
+    BX LR                //Return to C program
+FAIL:
+    MOVNE R0, #0         //If they are not equal, R0 is becomes = 0.
+    POP {R4}             //Removes access to R4
+    BX LR                //Returns to C
+
     
 
 
