d69eb199933fded5f6cccee289a93a7016ca812f
Benjamin Niccum <btn1609@mavs.uta.edu>
2023-10-05 19:40:28 -0500

    All descriptions complete

--- Homework4/HW4.s
+++ Homework4/HW4.s
@@ -58,58 +58,68 @@ loopl:                    //ends loop when string is entirely consumed
     POP {R4}              //resores original status to R4, removes from stack
     BX LR                 //Returns to C
 
-decimalStringToInt16:     //Converts string into 16 bit integer
+decimalStringToInt16:     //Converts numeric string into 16 bit integer
 //R0 = address of String passed into function, then 0, R1= Space to hold String, R2= Holds number 1
 //R3 = Holds char to be converted, R4= Holds number 10
+    PUSH {R4}             //Allows temporary access to R4
     MOV R1, R0            //Moves information in R0 into R1
     MOV R0, #0            //Sets R0 to zero
     MOV R2, #1            //Sets R2 to 1
-    MOV R4, #10           //Sets R4 to 10
+    MOV R4, #10           //Sets R4 to 10 in order to quickly multiply by ten to move decimal places
 dloop:
-    LDRSB R3, [R1], #1    //
-    CMP R3, #0
-    BEQ loopEnd
-    CMP R3, #48
-    BLT error
-    CMP R3, #57
-    BGT error
-    SUB R3, R3, #48
-    MUL R0, R0, R4
-    ADD R0, R0, R3
-    B dloop
-error:
-    MOV R0, #-1
-loopEnd:
-    BX LR
+    LDRSB R3, [R1], #1    //Loads the first nth letter of the string into R3 and moves address up by 1
+    CMP R3, #0            //checks if address is at null terminator, end of string
+    BEQ loopEnd           //If the string is consumed, jump to loopend
+    CMP R3, #48           //Compares letter of string to '0'<--Ascii 0, not null.  
+    BLT error             //If string letter is less than ascii 0, jump to error
+    CMP R3, #57           //Compare letter of string to '9' <---Ascii 9
+    BGT error             //If R3 is greate than 9, jump to error
+    SUB R3, R3, #48       //Subtract 48 from ascii value of R3 ex: (55-48)=(7-0)=7 
+    MUL R0, R0, R4        //Multiplies R0 by ten to move decimal places to tens place
+    ADD R0, R0, R3        //Adds R3 to ones place
+    B dloop               //Loops to add next symbol in string
+error:                    //Creates an error when symbol in string is not a number 0-9
+    MOV R0, #-1           //Places a -1 in R0 to indicate an error has occurred
+loopEnd:                  //ends the looping
+    POP {R4}              //Restores R4 to original conditions
+    BX LR                 //Returns to C
 
-hexStringToUint8:
-    MOV R1, R0
-    MOV R0, #0
-    MOV R2, #1
-    MOV R4, #16
-oloop:
-    LDRSB R3, [R1], #1
-    CMP R3, #0
-    BEQ loopEnd1
-    CMP R3, #48
-    BLT error1
-    CMP R3, #57
-    BGT hex1
-    SUB R3, R3, #48
-calc:
-    MUL R0, R0, R4
-    ADD R0, R0, R3
-    B oloop
-hex1:
-    CMP R3, #64
-    BGT hex2
-    BLE error1
-hex2:
-    CMP R3, #70
-    BGT error1
-    SUB R3, R3, #55
-    B calc
-error1:
-    MOV R0, #-1
-loopEnd1:
-    BX LR
+
+
+hexStringToUint8:          //Converts hex string into 8 bit integer
+//R0= string 
+PUSH {R4}                  //Gives acces to register 4
+    MOV R1, R0             //Move the string into R1 instead of R0
+    MOV R0, #0             //Move null into R0
+    MOV R2, #1             //Move 1 into R2
+    MOV R4, #16            //Hexidecimal requires 16 to move places, quick multiply by 16
+oloop:                     //ends loop when hex string is consumed
+    LDRSB R3, [R1], #1     //Loads first symbol in hexstring to R3 then post iterates R1
+    CMP R3, #0             //Checks to see if string is consumed
+    BEQ loopEnd1           //If string is consumed, jump to loopend1.
+    CMP R3, #48            //Compares hexstring symbol to Ascii 0
+    BLT error1             //If symbol is less than zero, jump to error
+    CMP R3, #57            //Compare hexstring symbol to ascii 9
+    BGT hex1               //If R3 is greater than 9, jump to hex1
+    SUB R3, R3, #48        //Place into R3 the non-string version of hexstring numeric
+calc:                      //
+//Fixed error here in provided solution.
+//Orig: MUL R0, R0, R4 <--- This does not work 
+//Fixed: MUL R0, R3, R4 <--- This should
+    MUL R0, R3, R4         //Moves symbol to next more significant hex-place
+    ADD R0, R0, R3         //Adds R3 to the least significant hex-place
+    B oloop                //Repeat process to finf end of string
+hex1:                      //This will test range for A,B,C,D,E,F values
+    CMP R3, #64            //Compares R3 to ascii A
+    BGT hex2               //If greater than ascii value @, the symbol before ascii value A, jump to hex 2
+    BLE error1             //If less than or equal to ascii @, jump to error
+hex2:                      //Tests max range for hexstring symbol
+    CMP R3, #70            //Compares R3 to ascii value F
+    BGT error1             //If greater than F, send to error
+    SUB R3, R3, #55        //Find numeric value for A=10, B=11, etc...
+    B calc                 //Jump to calc
+error1:                    //Occurs when symbol in hexstring is out of range
+    MOV R0, #-1            //Place -1 in R0 to indicate that an error has occured
+loopEnd1:                  //The program has worked successfully and needs to exit
+    POP {R4}               //Restores R4 to original condition
+    BX LR                  //Returns to C 
